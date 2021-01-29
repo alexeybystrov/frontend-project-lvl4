c1359997872343215b2cda30569a9fb7e0c3029d
Alexey Bystrov <alexeybystrov@me.com>
2021-01-29 23:06:42 +0300

    refactor channelsInfoSlice

--- src/slices/channelsInfoSlice.js
+++ src/slices/channelsInfoSlice.js
@@ -1,49 +1,34 @@
 import _ from 'lodash';
-import { createSlice, combineReducers } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-const channelsSlice = createSlice({
+const channelsInfoSlice = createSlice({
   name: 'channels',
   initialState: null,
   reducers: {
     receiveNewChannel: (state, { payload }) => {
       const newChannel = payload.data.attributes;
-      state.push(newChannel);
+      state.channels.push(newChannel);
     },
     removeChannel: (state, { payload }) => {
       const removedChannelId = payload.data.id;
-      _.remove(state, ({ id }) => id === removedChannelId);
+      _.remove(state.channels, ({ id }) => id === removedChannelId);
+      if (state.currentChannelId === removedChannelId) {
+        _.set(state, 'currentChannelId', 1);
+      }
     },
     renameChannel: (state, { payload }) => {
       const renamedChannelId = payload.data.id;
       const channelNewName = payload.data.attributes.name;
-      const channel = state.find(({ id }) => id === renamedChannelId);
+      const channel = state.channels.find(({ id }) => id === renamedChannelId);
       channel.name = channelNewName;
+    },
+    setCurrentChannelId: (state, { payload }) => {
+      _.set(state, 'currentChannelId', payload.id);
     },
   },
 });
 
-export const { receiveNewChannel, removeChannel, renameChannel } = channelsSlice.actions;
-
-const currentChannelIdSlice = createSlice({
-  name: 'currentChannelId',
-  initialState: null,
-  reducers: {
-    setCurrentChannelId: (state, { payload }) => payload.id,
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(removeChannel, (state, { payload }) => {
-        const currentChannelId = state;
-        const { id: removedId } = payload.data;
-        return currentChannelId === removedId ? 1 : state;
-      });
-  },
-});
-
-const channelsInfoReducer = combineReducers({
-  channels: channelsSlice.reducer,
-  currentChannelId: currentChannelIdSlice.reducer,
-});
-
-export const { setCurrentChannelId } = currentChannelIdSlice.actions;
-export default channelsInfoReducer;
+export const {
+  receiveNewChannel, removeChannel, renameChannel, setCurrentChannelId,
+} = channelsInfoSlice.actions;
+export default channelsInfoSlice.reducer;
